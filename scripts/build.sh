ccd4917aa64b097d502ca0f41cc708118cd4bce0
Ted <oancea.tudor@icloud.com>
2024-09-14 18:20:31 +0200

    add messsage definitinos

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+# Copyright (c) 2024, Tudor Oancea, Matteo Berthet
+# check that the script is run from the root of the ihm2 repository
+if [ $(basename "$PWD") != "brains2" ]; then
+    echo "Please run this script from the root of the ihm2 repository"
+    exit 1
+fi
+
+# makes sure that the correct python interpreter is called in CMake
+PYTHON_EXE=$(which python3)
+echo "Using python interpreter: $PYTHON_EXE"
+
+# run colcon build
+PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources colcon build --symlink-install --executor parallel --cmake-args -DCMAKE_BUILD_TYPE=Release -Wno-dev
