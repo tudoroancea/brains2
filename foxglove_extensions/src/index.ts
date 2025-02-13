772170a76d3514822442c45ac0938224d22dd098
Ted <oancea.tudor@icloud.com>
2025-02-13 15:27:22 +0100

    move stuff
    Signed-off-by: Ted <oancea.tudor@icloud.com>

--- foxglove_extensions/src/index.ts
+++ foxglove_extensions/src/index.ts
@@ -1,13 +1,7 @@
 import { ExtensionContext } from "@foxglove/extension";
 
 import { initCallServiceButton } from "./CallServiceButton";
-import { initExamplePanel } from "./SettingsPanel";
 
 export function activate(extensionContext: ExtensionContext): void {
-  // extensionContext.registerPanel({ name: "example-panel", initPanel: initExamplePanel });
   extensionContext.registerPanel({ name: "Call Service Button", initPanel: initCallServiceButton });
-  extensionContext.registerPanel({
-    name: "Foxglove Panel Settings Example",
-    initPanel: initExamplePanel,
-  });
 }
