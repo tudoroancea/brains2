8c9819df133f731d5cd05b067ffa551af4388dad
Ted <oancea.tudor@icloud.com>
2025-03-08 10:05:29 +0100

    ignore previous tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,4 @@
 # Copyright (c) 2024, Tudor Oancea, Matteo Berthet
-ctest --test-dir build/brains2 -O build/brains2/Testing/TestResults.log -V
+# ctest --test-dir build/brains2 -O build/brains2/Testing/TestResults.log
+colcon test --packages-select brains2
+colcon test-result --all --versbose
